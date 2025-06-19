9e9eb87ca59923aa138c75ba1b97d7f4b2b422ac
Kira <kirass0525@gmail.com>
2025-06-19 13:22:00 +0200

    update mapper and dto
    update mapper and dto for Shop User, add nested entity Favorite in Category

--- src/main/java/com/telran/store/mapper/ShopUserMapper.java
+++ src/main/java/com/telran/store/mapper/ShopUserMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",  uses = {FavoriteMapper.class})
 public interface ShopUserMapper {
 
     ShopUser toEntity(ShopUserCreateDto shopUserCreateDto);
